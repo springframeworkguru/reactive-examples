113bf024ec7d523573a4b94a2f6b0c3ec156be38
John Thompson <john@springframework.guru>
2021-02-28 11:53:03 -0500

    adding mono examples

---
+++ src/test/java/guru/springframework/reactiveexamples/PersonRepositoryImplTest.java
@@ -0,0 +1,58 @@
+package guru.springframework.reactiveexamples;
+
+import guru.springframework.reactiveexamples.domain.Person;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+
+class PersonRepositoryImplTest {
+
+    PersonRepositoryImpl personRepository;
+
+    @BeforeEach
+    void setUp() {
+        personRepository = new PersonRepositoryImpl();
+    }
+
+    @Test
+    void getByIdBlock() {
+        Mono<Person> personMono = personRepository.getById(1);
+
+        Person person = personMono.block();
+
+        System.out.println(person.toString());
+    }
+
+    @Test
+    void getByIdSubscribe() {
+        Mono<Person> personMono = personRepository.getById(1);
+
+        personMono.subscribe(person -> {
+            System.out.println(person.toString());
+        });
+    }
+
+    @Test
+    void getByIdMapFunction() {
+        Mono<Person> personMono = personRepository.getById(1);
+
+        personMono.map(person -> {
+            System.out.println(person.toString());
+
+            return person.getFirstName();
+        }).subscribe(firstName -> {
+            System.out.println("from map: " + firstName);
+        });
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
